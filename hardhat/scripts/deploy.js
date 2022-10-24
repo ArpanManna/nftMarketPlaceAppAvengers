90c56d1e2a2b93e9fab5c4daf6a444db8b9207e3
Arpan Manna <arpan.manna113@gmail.com>
2022-10-24 08:39:37 +0530

    Update deploy.js

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,16 +1,9 @@
 const { ethers } = require("hardhat");
 require("dotenv").config({ path: ".env" });
-//const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
+
 
 async function main() {
-  // Address of the whitelist contract that you deployed in the previous module
-  //const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
-  // URL from where we can extract the metadata for a Crypto Dev NFT
-  //const metadataURL = METADATA_URL;
-  /*
-  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-  so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
-  */
+  
   const marketplaceContract = await ethers.getContractFactory("Marketplace")
   const nftTokenContract = await ethers.getContractFactory("NftToken");
 
